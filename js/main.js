ec57d4e4b867472f855829456b7fc0366eae7d1b
miladMohammadi <afraaliansite@gmail.com>
2020-10-30 09:45:28 -0700

    js button menu mobile

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let menuBtn = document.querySelector('.hamburger-menu');
+
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen){
+        menuBtn.classList.add('open')
+        menuOpen = true
+    }
+    else {
+        menuBtn.classList.remove('open')
+        menuOpen = false
+    }
+});
+
+/*
+
+new Vue({
+    el: "#app",
+    data:{
+        menuOpen : false
+    },
+    methods:{
+        editClassMenu(){
+            if (!this.menuOpen){
+                menuBtn.classList.add('open')
+                this.menuOpen = true
+            }
+            else {
+                menuBtn.classList.remove('open')
+                this.menuOpen = false
+            }
+        }
+    }
+
+})*/
